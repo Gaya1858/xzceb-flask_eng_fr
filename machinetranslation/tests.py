fe3bf4a0d6730b66066fb48b8468b2299e9d1fa8
Gayathri Kanagaraj <53891739+Gaya1858@users.noreply.github.com>
2022-07-06 21:05:01 -0400

    Update tests.py

--- machinetranslation/tests.py
+++ machinetranslation/tests.py
@@ -1,20 +1,36 @@
+"""
+Testing Module
+"""
 import unittest
+import sys
+import os
 
-from translator import englishtofrench, frenchtoenglish
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
 
-   
+import translator as t
 
-class TestEnglishToFrench(unittest.TestCase):
 
-    def test1(self):
-       #self.assertEqual(frenchtoenglish(None),'')
-        self.assertEqual(frenchtoenglish('Bonjour'),'Hello')
+class TestTranslator(unittest.TestCase):
+    def test_nullInput_f2e(self):
+        self.assertEqual(t.french_to_english(""), "")
 
-class FrenchToEnglish(unittest.TestCase):
+    def test_HelloBonjour_f2e(self):
+        self.assertEqual(t.french_to_english("Bonjour"), "Hello")
 
-    def test2(self):
-        #self.assertEqual(englishtofrench(None),'')
-        self.assertEqual(englishtofrench('Hello'),'Bonjour')
+    def test_HelloBonjour_f2e_not(self):
+        self.assertNotEqual(t.french_to_english("Bonjour"), "Yes")
+
+    def test_nullInput_e2f(self):
+        self.assertEqual(t.english_to_french(""), "")
+
+    def test_BonjourHello_e2f(self):
+        self.assertEqual(t.english_to_french("Hello"), "Bonjour")
+
+    def test_BonjourHello_e2f_not(self):
+        self.assertNotEqual(t.english_to_french("Hello"), "J'Taime")
+
 
 if __name__ == '__main__':
     unittest.main()
